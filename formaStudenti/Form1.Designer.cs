25292529de5d196bfe15a41793c03d4a3b3721d6
Stefano Poropat <92926501+StefanoPoropat@users.noreply.github.com>
2024-09-23 13:18:15 +0200

    Add files via upload

---
+++ formaStudenti/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace formaStudenti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentDispaly = new System.Windows.Forms.ListBox();
+            this.newStudent = new System.Windows.Forms.Button();
+            this.deleteOneStudnet = new System.Windows.Forms.Button();
+            this.deleteAllStudents = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // studentDispaly
+            // 
+            this.studentDispaly.FormattingEnabled = true;
+            this.studentDispaly.Location = new System.Drawing.Point(92, 99);
+            this.studentDispaly.Name = "studentDispaly";
+            this.studentDispaly.Size = new System.Drawing.Size(260, 186);
+            this.studentDispaly.TabIndex = 0;
+            // 
+            // newStudent
+            // 
+            this.newStudent.Location = new System.Drawing.Point(92, 70);
+            this.newStudent.Name = "newStudent";
+            this.newStudent.Size = new System.Drawing.Size(157, 23);
+            this.newStudent.TabIndex = 1;
+            this.newStudent.Text = "Dodaj novog studenta";
+            this.newStudent.UseVisualStyleBackColor = true;
+            this.newStudent.Click += new System.EventHandler(this.newStudent_Click);
+            // 
+            // deleteOneStudnet
+            // 
+            this.deleteOneStudnet.Location = new System.Drawing.Point(358, 188);
+            this.deleteOneStudnet.Name = "deleteOneStudnet";
+            this.deleteOneStudnet.Size = new System.Drawing.Size(96, 23);
+            this.deleteOneStudnet.TabIndex = 2;
+            this.deleteOneStudnet.Text = "Obriši studneta";
+            this.deleteOneStudnet.UseVisualStyleBackColor = true;
+            this.deleteOneStudnet.Click += new System.EventHandler(this.deleteOneStudnet_Click);
+            // 
+            // deleteAllStudents
+            // 
+            this.deleteAllStudents.Location = new System.Drawing.Point(92, 291);
+            this.deleteAllStudents.Name = "deleteAllStudents";
+            this.deleteAllStudents.Size = new System.Drawing.Size(157, 23);
+            this.deleteAllStudents.TabIndex = 3;
+            this.deleteAllStudents.Text = "Obriši sve studente";
+            this.deleteAllStudents.UseVisualStyleBackColor = true;
+            this.deleteAllStudents.Click += new System.EventHandler(this.deleteAllStudents_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deleteAllStudents);
+            this.Controls.Add(this.deleteOneStudnet);
+            this.Controls.Add(this.newStudent);
+            this.Controls.Add(this.studentDispaly);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox studentDispaly;
+        private System.Windows.Forms.Button newStudent;
+        private System.Windows.Forms.Button deleteOneStudnet;
+        private System.Windows.Forms.Button deleteAllStudents;
+    }
+}
+
---
+++ formaStudenti/Properties/formName.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formaStudenti.Properties
+{
+    public partial class formName : Form
+    {
+        string name1, surname1, nuIndex1;
+
+        public formName()
+        {
+            InitializeComponent();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            name1 = txtName.Text;
+            surname1 = txtSurname.Text;
+            nuIndex1 = txtNuId.Text;
+        }
+
+        private void formName_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
