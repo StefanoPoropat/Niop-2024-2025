f9db76bf330ad94dbb26d768e10cc7cf05c03dbe
Stefano Poropat <92926501+StefanoPoropat@users.noreply.github.com>
2024-11-03 20:20:15 +0100

    Add files via upload

---
+++ pripremeZaTest/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace testRep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.addNewStudent = new System.Windows.Forms.Button();
+            this.deleteStudent = new System.Windows.Forms.Button();
+            this.deleteAll = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(50, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(301, 196);
+            this.listBox1.TabIndex = 0;
+            // 
+            // addNewStudent
+            // 
+            this.addNewStudent.Location = new System.Drawing.Point(50, 71);
+            this.addNewStudent.Name = "addNewStudent";
+            this.addNewStudent.Size = new System.Drawing.Size(193, 41);
+            this.addNewStudent.TabIndex = 1;
+            this.addNewStudent.Text = "Dodaj novog studenta";
+            this.addNewStudent.UseVisualStyleBackColor = true;
+            this.addNewStudent.Click += new System.EventHandler(this.addNewStudent_Click);
+            // 
+            // deleteStudent
+            // 
+            this.deleteStudent.Location = new System.Drawing.Point(357, 201);
+            this.deleteStudent.Name = "deleteStudent";
+            this.deleteStudent.Size = new System.Drawing.Size(193, 41);
+            this.deleteStudent.TabIndex = 2;
+            this.deleteStudent.Text = "Obrisi studenta";
+            this.deleteStudent.UseVisualStyleBackColor = true;
+            this.deleteStudent.Click += new System.EventHandler(this.deleteStudent_Click);
+            // 
+            // deleteAll
+            // 
+            this.deleteAll.Location = new System.Drawing.Point(50, 320);
+            this.deleteAll.Name = "deleteAll";
+            this.deleteAll.Size = new System.Drawing.Size(193, 41);
+            this.deleteAll.TabIndex = 3;
+            this.deleteAll.Text = "Obrisi sve studente";
+            this.deleteAll.UseVisualStyleBackColor = true;
+            this.deleteAll.Click += new System.EventHandler(this.deleteAll_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(357, 71);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(193, 41);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Učitaj";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(357, 320);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(193, 41);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Spremi";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.AutoSize = false;
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 466);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 30);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 24);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(151, 24);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 496);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.deleteAll);
+            this.Controls.Add(this.deleteStudent);
+            this.Controls.Add(this.addNewStudent);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button addNewStudent;
+        private System.Windows.Forms.Button deleteStudent;
+        private System.Windows.Forms.Button deleteAll;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ pripremeZaTest/addStud.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace testRep
+{
+    public partial class addStud : Form
+    {
+        public addStud()
+        {
+            InitializeComponent();
+        }
+
+        private DateTime pocetno = DateTime.Now;
+
+        private string study;
+        public string name => txbName.Text;
+        public string surname => txbSurname.Text;
+        public string index => txbIndex.Text;
+        public string direction => cbDirection.Text;
+        public string date => dtpDateOfBirth.Text;
+
+        public string studyType
+        {
+            get
+            {
+                if (rbRegular.Checked)
+                {
+                    study = "R";
+                }
+                if (rbIregular.Checked)
+                {
+                    study = "I";
+                }
+                return study;
+            }
+        }
+
+        private void btnCloseWithOutSaving_Click(object sender, System.EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnSaveData_Click(object sender, System.EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = new TimeSpan();
+            proteklo = DateTime.Now - pocetno;
+            toolStripStatusLabel1.Text = "Proteklo: " +
+            proteklo.Hours.ToString() + ":" + proteklo.Minutes.ToString() + ":" + proteklo.Seconds.ToString();
+        }
+    }
+}
