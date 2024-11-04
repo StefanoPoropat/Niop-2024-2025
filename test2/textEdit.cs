e48b0fbcd4fd2644cc9387b2020aeee1db1c5be6
Stefano Poropat <92926501+StefanoPoropat@users.noreply.github.com>
2024-11-04 13:14:54 +0100

    Add files via upload

---
+++ test2/textEdit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Windows.Forms.AxHost;
+
+namespace test2
+{
+    internal class textEdit
+    {
+        public string _text, _description;
+        private string text { get => _text; set => _text = value; }
+        private string description { get => _description; set => _description = value; }
+
+        public textEdit(string text, string description)
+        {
+           _text = text;
+           _description = description;
+        }
+        public override string ToString()
+        {
+            return $"{text}";
+        }
+        public string ToString2()
+        {
+            return $"{description}";
+        }
+
+
+    }
+}
